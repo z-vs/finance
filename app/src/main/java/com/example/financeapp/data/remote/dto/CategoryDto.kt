d26e6b0acbf9f2c32c5036b9c3251f9d33d34627
z-vs <zvs1110@gmail.com>
2026-05-30 09:57:22 +0300

    refactor: remove icon field from CategoryDto

--- app/src/main/java/com/example/financeapp/data/remote/dto/CategoryDto.kt
+++ app/src/main/java/com/example/financeapp/data/remote/dto/CategoryDto.kt
@@ -5,18 +5,15 @@ import com.google.gson.annotations.SerializedName
 data class CreateCategoryRequest(
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String,
-    @SerializedName("icon") val icon: String? = null
 )
 
 data class UpdateCategoryRequest(
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String,
-    @SerializedName("icon") val icon: String? = null
 )
 
 data class CategoryResponse(
     @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String,
-    @SerializedName("icon") val icon: String?
 )
